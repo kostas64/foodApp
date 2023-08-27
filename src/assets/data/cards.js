0d5190797ddf044df2f89369253a27d69c7db020
Konstantinos Efkarpidis <kostas11062@gmail.com>
2023-08-27 18:02:41 +0300

    add extra cards to test functionality

--- src/assets/data/cards.js
+++ src/assets/data/cards.js
@@ -9,4 +9,19 @@ export const cards = [
     expDate: '02/27',
     name: 'Maria Ioannidou',
   },
+  {
+    digits: '3672',
+    expDate: '08/26',
+    name: 'Kim Kardashian',
+  },
+  {
+    digits: '2987',
+    expDate: '01/29',
+    name: 'Jason Statham',
+  },
+  {
+    digits: '7216',
+    expDate: '11/28',
+    name: 'John Papadopoulos',
+  },
 ];
