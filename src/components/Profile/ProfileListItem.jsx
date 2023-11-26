82bc359be965324e039747845fb492223053ac12
Konstantinos Efkarpidis <kostas11062@gmail.com>
2023-11-26 18:04:29 +0200

    profile, list, items ui

---
+++ src/components/Profile/ProfileListItem.jsx
@@ -0,0 +1,68 @@
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  Dimensions,
+  TouchableOpacity,
+} from 'react-native';
+import React from 'react';
+import {useTheme} from '@react-navigation/native';
+
+import {images} from '../../constants';
+import {DimensionsUtils} from '../../utils/DimensionsUtils';
+
+const {width} = Dimensions.get('screen');
+
+const ProfileListItem = ({item, isLast}) => {
+  const {colors} = useTheme();
+  const styles = customStyle(colors);
+
+  return (
+    <>
+      <TouchableOpacity style={[styles.container, styles.rowCenter]}>
+        <View style={styles.rowCenter}>
+          <Image
+            source={item.icon}
+            style={[styles.image, styles.marginRight]}
+          />
+          <Text style={styles.label}>{item.label}</Text>
+        </View>
+        <Image source={images.arrowLong} style={styles.image} />
+      </TouchableOpacity>
+      {!isLast && <View style={styles.hr} />}
+    </>
+  );
+};
+
+const customStyle = colors =>
+  StyleSheet.create({
+    rowCenter: {
+      flexDirection: 'row',
+      alignItems: 'center',
+    },
+    container: {
+      justifyContent: 'space-between',
+    },
+    image: {
+      tintColor: colors.orange,
+      width: DimensionsUtils.getDP(20),
+      height: DimensionsUtils.getDP(20),
+    },
+    label: {
+      color: colors.text,
+      fontFamily: 'Poppins-Regular',
+    },
+    hr: {
+      marginVertical: DimensionsUtils.getDP(16),
+      left: DimensionsUtils.getDP(36),
+      width: width - DimensionsUtils.getDP(76),
+      height: 1,
+      backgroundColor: colors.orange,
+    },
+    marginRight: {
+      marginRight: DimensionsUtils.getDP(16),
+    },
+  });
+
+export default ProfileListItem;
--- src/screens/ProfileScreen.jsx
+++ src/screens/ProfileScreen.jsx
@@ -1,19 +1,45 @@
 import React from 'react';
-import {StyleSheet, Text} from 'react-native';
+import {FlatList, StyleSheet} from 'react-native';
 
 import Screen from '../components/Common/Screen';
+import Header from '../components/Common/Header';
 import useBackAction from '../hooks/useBackAction';
+import {PROFILE_ITEMS} from '../assets/profileItems';
+import {DimensionsUtils} from '../utils/DimensionsUtils';
+import ProfileListItem from '../components/Profile/ProfileListItem';
 
 const ProfileScreen = () => {
   useBackAction();
 
+  const renderItem = React.useCallback(
+    ({item, index}) => (
+      <ProfileListItem
+        key={index}
+        item={item}
+        isLast={index === PROFILE_ITEMS.length - 1}
+      />
+    ),
+    [],
+  );
+
   return (
     <Screen>
-      <Text>ProfileScreen</Text>
+      <Header label={'Profile'} />
+      <FlatList
+        bounces={false}
+        data={PROFILE_ITEMS}
+        renderItem={renderItem}
+        style={styles.list}
+      />
     </Screen>
   );
 };
 
-export default ProfileScreen;
+const styles = StyleSheet.create({
+  list: {
+    paddingTop: DimensionsUtils.getDP(16),
+    paddingHorizontal: DimensionsUtils.getDP(20),
+  },
+});
 
-const styles = StyleSheet.create({});
+export default ProfileScreen;
