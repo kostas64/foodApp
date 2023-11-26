2b7ae602b9057bf944cb711f53108f4c7f20ff2d
Konstantinos Efkarpidis <kostas11062@gmail.com>
2023-11-26 15:53:47 +0200

    navigation util

--- src/utils/GenericUtils.js
+++ src/utils/GenericUtils.js
@@ -1,4 +1,5 @@
 import {Linking} from 'react-native';
+import {CommonActions} from '@react-navigation/native';
 
 export const openURL = async url => {
   const supported = await Linking.canOpenURL(url);
@@ -7,5 +8,29 @@ export const openURL = async url => {
     await Linking.openURL(url);
   } else {
     console.log(`Cant open ${url} - Error :`, e);
+  }
+};
+
+export const navigateBack = (navigation, from) => {
+  if (from === 'Search') {
+    navigation.dispatch(
+      CommonActions.reset({
+        index: 0,
+        routes: [
+          {
+            name: 'HomeStack',
+            state: {
+              routes: [
+                {
+                  name: 'Home',
+                },
+              ],
+            },
+          },
+        ],
+      }),
+    );
+  } else {
+    navigation.pop();
   }
 };
